0bc15ef2af38ff9b8a8e57b8fb4d18786edba897
kunal garg <kunalg5802@gmail.com>
2023-08-26 21:27:04 +0530

    set routes and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,30 @@
 import { BrowserRouter, Routes, Navigate, Route } from "react-router-dom";
+import LoginPage from "scenes/loginpage";
+import ProfilePage from "scenes/profilePage";
+import HomePage from "scenes/homePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
