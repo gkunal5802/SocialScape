f117473be2cb1f704fa17042f489fc246322b452
kunal garg <kunalg5802@gmail.com>
2023-08-28 14:54:52 +0530

    created widgets style

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+
+const UserImage = ({ image, size = "60px" }) => {
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        alt="user"
+        width={size}
+        height={size}
+        src={`http://localhost:3000/assets/${image}`}
+      />
+    </Box>
+  );
+};
+
+export default UserImage;
