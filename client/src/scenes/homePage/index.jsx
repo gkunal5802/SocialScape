ac8e2aa286ea321e0238387d110a3d2df401decf
kunal garg <kunalg5802@gmail.com>
2023-08-28 21:20:07 +0530

    integration userWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,32 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import NavBar from "../navBar/index.jsx";
+import UserWidget from "scenes/widgets/userWidgets.jsx";
+import { useSelector } from "react-redux";
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
       <NavBar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        gap="0.5rem"
+        display={isNonMobileScreens ? "flex" : "block"}
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        ></Box>
+
+        {isNonMobileScreens && <Box flexBasis="26%"></Box>}
+      </Box>
     </Box>
   );
 };
--- client/src/scenes/widgets/userWidgets.jsx
+++ client/src/scenes/widgets/userWidgets.jsx
@@ -13,7 +13,8 @@ import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const UserWidget = async ({ userId, picturePath }) => {
+const UserWidget = ({ userId, picturePath }) => {
+  console.log(userId);
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const { palette } = useTheme();
@@ -23,11 +24,10 @@ const UserWidget = async ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const response = await fetch("http://localhost:3000/users/:userId", {
+    const response = await fetch(`http://localhost:3000/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
-
     const data = await response.json();
     setUser(data);
   };
@@ -45,7 +45,7 @@ const UserWidget = async ({ userId, picturePath }) => {
     location,
     friends,
     occupation,
-    profileViewed,
+    viewedProfile,
     impressions,
   } = user;
 
@@ -75,68 +75,70 @@ const UserWidget = async ({ userId, picturePath }) => {
             >
               {firstName} {lastName}
             </Typography>
-            <Typography color={medium}>{friends.length} friends</Typography>
+            <Typography color={medium}>{friends?.length} friends</Typography>
           </Box>
-          <ManageAccountsOutlined />
+        </FlexBetween>
+        <ManageAccountsOutlined />
+      </FlexBetween>
+      <Divider />
+
+      {/* SECOND ROW */}
+      <Box p="1rem 0">
+        <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+          <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{location}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center" gap="1rem">
+          <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+          <Typography color={medium}>{occupation}</Typography>
+        </Box>
+      </Box>
+
+      <Divider />
+      {/* THIRD ROW */}
+      <Box p="1rem 0">
+        <FlexBetween mb="0.5rem">
+          <Typography color={medium}>Who's viewed your profile</Typography>
+          <Typography color={main} fontWeight="500">
+            {viewedProfile}
+          </Typography>
+        </FlexBetween>
+        <FlexBetween mb="0.5rem">
+          <Typography color={medium}>Impressions of Your Post</Typography>
+          <Typography color={main} fontWeight="500">
+            {impressions}
+          </Typography>
+        </FlexBetween>
+      </Box>
+
+      <Divider />
+      {/* FOURTH */}
+      <Box p="1rem 0">
+        <Typography fontSize="1rem" mb="1rem" color={main} fontWeight="500">
+          Social Profiles
+        </Typography>
+        <FlexBetween gap="1rem" mb="0.5rem">
+          <FlexBetween gap="1rem">
+            <img src="../assets/twitter.png" alt="twitter" />
+            <Typography color={main} fontWeight="500">
+              Twitter
+            </Typography>
+            <Typography color={medium}>Social Network</Typography>
+          </FlexBetween>
+          <EditOutlined sx={{ color: main }} />
         </FlexBetween>
 
-        <Divider />
-        {/* SECOND ROW */}
-        <Box p="1rem 0">
-          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
-            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
-            <Typography color={medium}>{location}</Typography>
-          </Box>
-          <Box display="flex" alignItems="center" gap="1rem">
-            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
-            <Typography color={medium}>{occupation}</Typography>
-          </Box>
-        </Box>
-
-        {/* THIRD ROW */}
-        <Box p="1rem 0">
-          <FlexBetween mb="0.5rem">
-            <Typography color={medium}>Who's viewed your profile</Typography>
-            <Typography color={main} fontWeight="500">
-              {profileViewed}
-            </Typography>
-          </FlexBetween>
-          <FlexBetween mb="0.5rem">
-            <Typography color={medium}>Impressions of Your Post</Typography>
-            <Typography color={main} fontWeight="500">
-              {impressions}
-            </Typography>
-          </FlexBetween>
-        </Box>
-
-        {/* FOURTH */}
-        <Box p="1rem 0">
-          <Typography fontSize="1rem" mb="1rem" color={main} fontWeight="500">
-            Social Profiles
-          </Typography>
-          <FlexBetween gap="1rem" mb="0.5rem">
-            <FlexBetween gap="1rem">
-              <img src="../assets/twitter.png" alt="twitter" />
-              <Typography color={main} fontWeight="500">
-                Twitter
-              </Typography>
-              <Typography color={medium}>Social Network</Typography>
-            </FlexBetween>
-            <EditOutlined sx={{ color: main }} />
-          </FlexBetween>
-
+        <FlexBetween gap="1rem">
           <FlexBetween gap="1rem">
-            <FlexBetween gap="1rem">
-              <img src="../assets/linkedin.png" alt="linkedin" />
-              <Typography color={main} fontWeight="500">
-                LinkedIn
-              </Typography>
-              <Typography color={medium}>Network Platform</Typography>
-            </FlexBetween>
-            <EditOutlined sx={{ color: main }} />
+            <img src="../assets/linkedin.png" alt="linkedin" />
+            <Typography color={main} fontWeight="500">
+              LinkedIn
+            </Typography>
+            <Typography color={medium}>Network Platform</Typography>
           </FlexBetween>
-        </Box>
-      </FlexBetween>
+          <EditOutlined sx={{ color: main }} />
+        </FlexBetween>
+      </Box>
     </WidgetWrapper>
   );
 };
